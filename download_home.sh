22a17331a6f016bb7f2c66729364846224c85c9a
dimitrijes5 <dimitrije@mxd.digital>
2025-05-15 16:00:29 +0200

    Tried again

---
+++ download_home.sh
@@ -0,0 +1,9 @@
+wget \
+  -p               
+  -k                
+  -E                
+  -l 1              
+  -np               
+  -D sydneychiropractorcbd.com.au 
+https://sydneychiropractorcbd.com.au/
+
