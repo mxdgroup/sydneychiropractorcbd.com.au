db37be059f826611cde3bf4574054c71c4fee1b6
dxmv <dimitirjestepanovic1@gmail.com>
2025-06-30 10:50:11 +0200

    Some fixes

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
-  integrations: [icon()],
+  integrations: [tailwind(), icon()],
 });
---
+++ astro/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'custom-pink': '#ed2279',
+        'custom-purple': '#1F2BA3',
+        'text': '#7A7A7A',
+        'custom-dark': '#333b55',
+      },
+      container: {
+        center: true,
+        padding: '2rem',
+        screens: {
+          '2xl': '1400px',
+        },
+      },
+    },
+  },
+  plugins: [],
+} 
