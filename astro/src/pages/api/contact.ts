0f0fb51393d708964ac0756cefd8984f15085199
dxmv <dimitirjestepanovic1@gmail.com>
2025-07-13 12:49:14 +0200

    Added orbit

--- astro/src/pages/api/contact.ts
+++ astro/src/pages/api/contact.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from 'astro';
 import { Resend } from 'resend';
 
-const resend = new Resend(import.meta.env.RESEND_API_KEY);
+const resend = new Resend("re_43MSkjeo_9NfhgUUsivNPY4YuUWohCtyv");
 
 export const prerender = false;
 
