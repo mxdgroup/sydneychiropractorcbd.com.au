0c27e2c3e97d5393965be9de159b6172920e5775
dimitrijes5 <dimitrije@mxd.digital>
2025-09-08 16:10:13 +0200

    maybe fix

--- astro/tailwind.config.js
+++ astro/tailwind.config.js
@@ -8,6 +8,13 @@ export default {
         'custom-purple': '#1F2BA3',
         'text': '#7A7A7A',
         'custom-dark': '#333b55',
+        // Adding CSS custom properties from BookLayout
+        'primary-blue': 'rgb(0, 86, 179)',
+        'primary-pink': 'rgba(223,47,109)',
+        'purple': 'rgb(32, 32, 137)',
+        'text-dark': '#333333',
+        'text-light': '#ffffff',
+        'link-hover': '#0e1346',
       },
       container: {
         center: true,
